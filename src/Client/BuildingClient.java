89afffc87898a82c0a224a3ae51cad5558d7974b
lasking88 <j.k.oh@student.rug.nl>
2018-02-25 00:37:19 +0100

    Initial commit

---
+++ src/Client/BuildingClient.java
@@ -0,0 +1,11 @@
+package Client;
+
+/**
+ * Created by jk on 24/02/18.
+ */
+public class BuildingClient {
+
+    public static void main(String argv[]) {
+        System.out.println("I'm a building client.");
+    }
+}
---
+++ src/Server/Server.java
@@ -0,0 +1,11 @@
+package Server;
+
+/**
+ * Created by jk on 24/02/18.
+ */
+public class Server {
+
+    public static void main(String [] args)  {
+        System.out.println("I'm server.");
+    }
+}
