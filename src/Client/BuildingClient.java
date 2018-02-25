acba2b55dfdad8afcdb2aa086fc0d7aa7598e76a
lasking88 <j.k.oh@student.rug.nl>
2018-02-25 12:56:33 +0100

    Basic Socket programming

--- src/Client/BuildingClient.java
+++ src/Client/BuildingClient.java
@@ -1,11 +1,48 @@
 package Client;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Created by jk on 24/02/18.
  */
 public class BuildingClient {
 
+    private static final String DATA = "This is building client. I'm trying to send this message to client.";
+    private static final String ADDRESS = "localhost";
+    private static final int PORT = 9900;
+
+    private Socket buildingSocket;
+    private DataOutputStream out;
+    private DataInputStream in;
+
+    public BuildingClient() {
+        try {
+            initializeBuilding();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeBuilding() throws IOException {
+        buildingSocket = new Socket(ADDRESS, PORT);
+        System.out.println("Connected");
+        in = new DataInputStream(buildingSocket.getInputStream());
+        out = new DataOutputStream(buildingSocket.getOutputStream());
+
+        out.writeBytes(DATA);
+
+        out.close();
+        in.close();
+        buildingSocket.close();
+    }
+
     public static void main(String argv[]) {
         System.out.println("I'm a building client.");
+        BuildingClient client = new BuildingClient();
     }
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,11 +1,54 @@
 package Server;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by jk on 24/02/18.
  */
 public class Server {
 
+    private static final int PORT = 9900;
+
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private DataInputStream in;
+    private ByteArrayOutputStream out;
+
+    public Server() {
+        try {
+            initializeServer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeServer() throws IOException {
+        serverSocket = new ServerSocket(PORT);
+        System.out.println("Server started");
+        System.out.println("Waiting for a client ...");
+        clientSocket = serverSocket.accept();
+        System.out.println("Client accepted");
+
+        in = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+        out = new ByteArrayOutputStream();
+
+        int bytesRead = 0;
+        byte[] buffer = new byte[1024];
+        while ((bytesRead = in.read(buffer)) > 0) {
+            out.write(buffer, 0, bytesRead);
+        }
+        System.out.println("Closing connection");
+        System.out.println(new String(out.toByteArray()));
+
+        clientSocket.close();
+        in.close();
+        out.close();
+    }
+
     public static void main(String [] args)  {
         System.out.println("I'm server.");
+        Server server = new Server();
     }
 }
